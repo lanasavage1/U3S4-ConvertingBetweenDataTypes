b558f2459b516c1cf0a6cb34d7a88476eba9c14c
lanasavage1 <lanasavage25@gmail.com>
2024-12-10 15:59:49 -0500

    Update Casting01.java

--- src/Casting01.java
+++ src/Casting01.java
@@ -2,7 +2,7 @@ public class Casting01 {
     public static void main(String[] args) {
         //Declare and initialize a byte with a value of 128
         //Observe NetBeans' complaint
-
+//hi//
         
         //Declare and initialize a short with a value of 128
         //Create a print statement that casts this short to a byte
